86c11c8b9e5c429ba033a3c3ec6b5e4019fce529
Saran Kumar <145222035+Sarankumar96@users.noreply.github.com>
2024-01-30 19:04:32 +0530

    added html and js files

---
+++ Primitive datatypes.js
@@ -0,0 +1,11 @@
+// 
+let a = 5;
+a=6;
+let b = a;
+a =1; 
+ 
+b = 8 ;
+
+console.log(a);
+console.log(b);  
+
---
+++ ref.js
@@ -0,0 +1,14 @@
+let person = {
+    name : 'saran',
+    class : 'bsc',
+    rollNo : 36
+} 
+
+let secondPerson = person;
+
+
+person.name = 'kumar';
+secondPerson.name = 'premi';
+
+console.log(person);
+console.log(secondPerson); 
